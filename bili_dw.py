e8dd044c4949caf3aa4e84242ff1173658d93376
kirito <1456457523@qq.com>
2018-03-17 12:49:44 +0800

    添加了外部调用模式

--- bili_dw.py
+++ bili_dw.py
@@ -2,6 +2,7 @@ import requests
 import re
 import os
 import logging
+import sys
 
 
 reg = r'<meta data-vue-meta="true" itemprop="thumbnailUrl" content="(.*?)"/>'
@@ -16,9 +17,9 @@ def gethtml(url):
     except:
        logging.error("网页下载失败")
 
-def download(url,root):
+def download(url,root,name):
     #path = root + url.split('/')[-1]
-    path = root + 'cover' + '.' + url.split('.')[-1]
+    path = root + name + '.' + url.split('.')[-1]
     try:
         if not os.path.exists(root):
             os.mkdir(root)
@@ -28,11 +29,11 @@ def download(url,root):
             with open(path, 'wb') as f:
                 f.write(r.content)
                 f.close()
-                logging.info("封面保存成功%s" % path)
-                return "封面保存成功%s" % path
+                logging.info("封面保存成功 %s" % path)
+                return "封面保存成功 %s" % path
         else:
-            logging.warning("封面已存在%s" % path)
-            return "封面已存在%s" % path
+            logging.warning("封面已存在 %s" % path)
+            return "封面已存在 %s" % path
     except:
         logging.error("封面下载失败")
         return "封面下载失败"
@@ -42,11 +43,33 @@ def ds(reg,a):
         #print(i)
         return(i)
 
-
+#内部调用代码
 def bi(num,root):
     a = gethtml("https://www.bilibili.com/video/av%s/"%(num))
     url = ds(reg, a)
     logging.info(url)
-    info = download(url, root)
+    info = download(url, root, "cover")
     return info
+
+#外部调用代码
+if __name__ =='__main__':
+    locate = 'D://pics//'
+    av = sys.argv[1]
+    name = av
+    #print(sys.argv)
+    if len(sys.argv) > 2:
+        if sys.argv.count('-l'):
+            num = sys.argv.index('-l')
+            locate = sys.argv[num + 1]
+        if sys.argv.count('-n'):
+            num = sys.argv.index('-n')
+            name = sys.argv[num + 1]
+        if sys.argv.count('-r'):
+            num = sys.argv.index('-r')
+            reg = sys.argv[num + 1]
+    #print(av, root, name)
+    html = gethtml("https://www.bilibili.com/video/av%s/"%(av))
+    url = ds(reg, html)
+    info = download(url, locate, name)
+    print(info)
 
