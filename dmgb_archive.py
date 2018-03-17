592768918895c0f7f676d14dd3cbf172ef96ce1a
kirito <1456457523@qq.com>
2018-03-17 11:18:19 +0800

    增加了多进程的支持，修改了日志的记录内容

--- dmgb_archive.py
+++ dmgb_archive.py
@@ -1,12 +1,13 @@
 # coding=gbk
-import os, sys, re, logging, time, subprocess, shutil, requests
+import time
+import os, sys, re, logging, time, subprocess, shutil
 from bili_dw import *
+from multiprocessing import Pool
 
 reg1 = r'《(.*?)》'
 reg2 = r'第(.*?)期'
-reg3 = r'<meta data-vue-meta="true" itemprop="thumbnailUrl" content="(.*?)"/>'
+#reg3 = r'<meta data-vue-meta="true" itemprop="thumbnailUrl" content="(.*?)"/>'
 reg4 = r'Av(.*?),'
-#reg5 = r''
 reg5 = r'[0-9].(.*?)A'
 
 homedir = os.getcwd()
@@ -16,23 +17,31 @@ work_locate = homedir
 support_list = ['mp4']
 
 
-logging.basicConfig(level = logging.DEBUG, format = '%(levelname)s - %(asctime)s - %(message)s')
-logger = logging.getLogger(__name__)
-logger.setLevel(level = logging.INFO)
-
+logger = logging.getLogger("test.txt")   #创建一个logger,默认为root logger
+logger.setLevel(logging.DEBUG)   #设置全局log级别为debug。注意全局的优先级最高
+hterm =  logging.StreamHandler()    #创建一个终端输出的handler,设置级别为error
+hterm.setLevel(logging.INFO)
+hfile = logging.FileHandler("access.log")    #创建一个文件记录日志的handler,设置级别为info
+hfile.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')   #创建一个全局的日志格式
+format_cs = logging.Formatter('%(levelname)s - %(asctime)s - %(message)s')
+hterm.setFormatter(format_cs)   #将日志格式应用到终端handler
+hfile.setFormatter(formatter)   #将日志格式应用到文件handler
+logger.addHandler(hterm)    #将终端handler添加到logger
+logger.addHandler(hfile)    #将文件handler添加到logger
 
 def check_diary(dir):
     check1 = os.path.exists(dir)
     if check1:
-        logging.info("%s文件夹已找到" % dir)
+        logger.info("%s文件夹已找到" % dir)
     else:
-        logging.info("未发现%s文件夹，创建... " % dir)
+        logger.info("未发现%s文件夹，创建... " % dir)
         try:
             os.makedirs(dir)
-            logging.info("创建成功")
+            logger.info("创建成功")
         except:
-            logging.error("创建失败，请检查权限")
-            input()
+            logger.error("创建失败，请检查权限")
+            time.sleep(10)
             sys.exit()
 
 def file_rename(name):
@@ -60,41 +69,51 @@ def file_rename(name):
         new_name = m_name + term_name + suffix
         #print(new_name)
 
+        logger.debug('Run task %s (%s)...' % (new_name, os.getpid()))
+        start = time.time()
         os.rename(name, new_name)
-        logging.info('文件  %s  重命名为  %s' % (name, new_name))
+        logger.info('文件  %s  重命名为  %s' % (name, new_name))
         check_diary(mp3_dir + '\\' + m_name)
         check_diary(mp4_dir + '\\' + m_name)
 
-        for z in re.findall(reg4,name):
-            print(z)
-            bi(z,mp3_dir + '\\' + m_name + '\\')
-            bi(z, mp4_dir + '\\' + m_name + '\\')
+        for z in re.findall(reg4, name):
+            #print(z)
+            bili_info = bi(z, mp3_dir + '\\' + m_name + '\\')
+            logger.info(bili_info)
+            # bi(z, mp4_dir + '\\' + m_name + '\\')
+            #采用移动文件的方式减小服务器负载
+            shutil.copy(mp3_dir + '\\' + m_name + '\\' + 'cover.jpg',mp4_dir + '\\' + m_name + '\\' + 'cover.jpg')
 
-        code = bulid_cfg(new_name, mp3_dir + '\\' +m_name+ '\\' + new_name.replace('mp4', 'mp3'))
+
+        code = bulid_cfg(new_name, mp3_dir + '\\' + m_name + '\\' + new_name.replace('mp4', 'mp3'))
 
         if code:
             shutil.move(new_name, mp4_dir + '\\' + m_name)
+        end = time.time()
+        logger.debug('Task %s runs %0.2f seconds.\n' % (new_name, (end - start)))
 
 def bulid_cfg(name, locate):
     cfg = 'ffmpeg -i data1 -vn  -acodec libmp3lame -ac 2 -qscale:a 4 -ar 48000  data2'
     cfg = cfg.replace('data1', name)
     cfg = cfg.replace('data2', locate)
-    logging.info(cfg)
+    logger.debug(cfg)
     returnCode = subprocess.call(cfg, shell=True, stdin=subprocess.PIPE)
     if not returnCode:
-        logging.info('%s转码成功' %name)
+        logger.info('%s转码成功' %name)
         return 1
     else:
-        logging.error('%s转码失败，命令为 %s' % (name, cfg))
+        logger.error('%s转码失败，命令为 %s' % (name, cfg))
 
-
-check_diary(mp3_dir)
-check_diary(mp4_dir)
-for i in os.listdir(work_locate):
-    for k in support_list:
-        if k in i:
-            file_rename(i)
-            for x in re.findall(reg1, i):
-                print(x)
+if __name__=='__main__':
+    check_diary(mp3_dir)
+    check_diary(mp4_dir)
+    p = Pool()
+    for i in os.listdir(work_locate):
+        for k in support_list:
+            if k in i:
+                p.apply_async(file_rename, args=(i,))
+    p.close()
+    p.join()
+    logging.info("所有任务完成")
 
 
